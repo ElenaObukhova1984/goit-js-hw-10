f98f1a784b0a053c9d378962176d2109f8649fcf
ElenaObukhova1984 <e.obukhova84@gmail.com>
2022-10-08 23:23:31 +0300

    11

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 
 const DEBOUNCE_DELAY = 300;
+const debounce = require("lodash.debounce");
+
+const refs = {
+    input: document.querySelector("#search-box"),
+    list: document.querySelector(".country-list"),
+    container: document.querySelector(".country-info"),
+};
+// console.log(refs.input);
+
+const handleInput = (event) => {
+    event.preventDefault();
+    const country = event.target.value.trim().toLowerCase();
+    console.log(country);
+
+    if (country === "") {
+        Notify.failure("Please, enter data to search!!!");
+        return (refs.list.innerHTML = ""),
+            (refs.container.innerHTML = "");
+    };
+
+    // console.log('get');
+};
+
+refs.input.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
+
